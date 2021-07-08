f709a7c8e7cd93d53da47e88ac0621dd112f24a9
Minhyeong Jang <public.doriri@gmail.com>
2021-07-08 14:25:54 +0900

    feat: 미사용 코드 제거

--- js/vaccine.js
+++ js/vaccine.js
@@ -1,9 +1,6 @@
-/**
- * 검색 버튼 클릭
- */
 $("#position").keypress(function (e) {
   if (e.which == 13) {
-    getVaccine(); // 실행할 이벤트
+    getVaccine();
   }
 });
 $("button.search").on("click", () => {
@@ -16,9 +13,6 @@ $("button.popup-test").on("click", () => {
   );
 });
 
-/**
- * 백신 정보
- */
 let keywordItems = {};
 let collapseCount = 0;
 
@@ -100,61 +94,6 @@ const getVaccine = () => {
               }
             },
           });
-          // var xhr = new XMLHttpRequest();
-          // xhr.open(
-          //   "POST",
-          //   "https://vaccine.kakao.com/api/v2/vaccine/left_count_by_coords",
-          //   true
-          // );
-          // xhr.setRequestHeader("Content-Type", "application/json");
-          // xhr.onload = () => {
-          //   if (xhr.status === 200 || xhr.status === 201) {
-          //     $(`.loading-${loadingKeyword}`).toggleClass("active");
-
-          //     JSON.parse(xhr.responseText).organizations.map((item) => {
-          //       if (!keywordItems[keyword]) {
-          //         $(
-          //           `<div class="org-item"><div class="org-name">${item.orgName}</div><div class="address">${item.address}</div></div>`
-          //         ).appendTo(collapseItem);
-          //       }
-          //       if (item.leftCounts) {
-          //         console.log(
-          //           `성공했어요 : https://vaccine.kakao.com/reservation/${item.orgCode}?from=KakaoMap&code=VEN00013`
-          //         );
-          //         const successList = $(".success-list");
-          //         const contentLi = $("<li>").appendTo(successList);
-          //         $(
-          //           `<a href="${`https://vaccine.kakao.com/reservation/${item.orgCode}?from=KakaoMap&code=VEN00013`}" target="_blank">${keyword} : ${
-          //             item.orgName
-          //           }</a>`
-          //         ).appendTo(contentLi);
-
-          //         soundManager.onready(() => {
-          //           soundManager.createSound({
-          //             id: "mySound",
-          //             url: "/doorbell.wav",
-          //           });
-          //           soundManager.play("mySound");
-          //         });
-          //         window.open(
-          //           `https://vaccine.kakao.com/reservation/${item.orgCode}?from=KakaoMap&code=VEN00013`,
-          //           "_blank"
-          //         );
-          //       }
-          //     });
-          //     keywordItems[keyword] = true;
-          //   } else {
-          //     console.error(xhr.responseText);
-          //   }
-          // };
-          // xhr.send(
-          //   JSON.stringify({
-          //     bottomRight: { x: location.lng - 0.01, y: location.lat + 0.02 },
-          //     onlyLeft: false,
-          //     order: "latitude",
-          //     topLeft: { x: location.lng + 0.01, y: location.lat - 0.02 },
-          //   })
-          // );
         }, 1000);
         collapseCount++;
       }
