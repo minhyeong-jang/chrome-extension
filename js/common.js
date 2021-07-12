44736ce6d1746c1125c0d0197a7d08ba6ff71243
Minhyeong Jang <public.doriri@gmail.com>
2021-07-12 19:56:01 +0900

    feat: UI 및 함수 개선

--- js/common.js
+++ js/common.js
@@ -1,24 +1,40 @@
 var lib = lib || {};
 
+lib.ajaxSubmit = ({ url, type, data, beforeSend }) => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: url,
+      type: type,
+      data: data,
+      beforeSend: beforeSend,
+      success: (data) => resolve(data),
+      error: (e) => reject(e),
+      cache: false,
+      contentType: false,
+      processData: false,
+    });
+  });
+};
+
 /**
  * Ajax 간편화
  * lib.ajaxSubmit('https://test.co.kr', 'post', {a:'1',b:'2'}, function(status, message){ ... });
  * return results, status
  */
-lib.ajaxSubmit = ({ url, type, data, beforeSend, callback }) => {
-  $.ajax({
-    url: url,
-    type: type,
-    data: data,
-    beforeSend: beforeSend,
-    success: (data) => {
-      callback(data);
-    },
-    cache: false,
-    contentType: false,
-    processData: false,
-  });
-};
+// lib.ajaxSubmit = ({ url, type, data, beforeSend, callback }) => {
+//   $.ajax({
+//     url: url,
+//     type: type,
+//     data: data,
+//     beforeSend: beforeSend,
+//     success: (data) => {
+//       callback(data);
+//     },
+//     cache: false,
+//     contentType: false,
+//     processData: false,
+//   });
+// };
 
 /**
  * JSON -> URL Parameter
--- js/vaccine.js
+++ js/vaccine.js
@@ -7,97 +7,106 @@ $("button.search").on("click", () => {
   getVaccine();
 });
 $("button.popup-test").on("click", () => {
-  window.open(
-    `https://vaccine.kakao.com/reservation/123?from=KakaoMap&code=VEN00013`,
-    "_blank"
-  );
+  window.open(`https://vaccine.kakao.com/reservation/123`, "_blank");
 });
 
 let keywordItems = {};
-let collapseCount = 0;
+let count = 0;
 
-const getVaccine = () => {
+const getCoords = async (keyword) => {
+  const params = lib.jsonToParameter({
+    address: keyword,
+    key: "AIzaSyDZA26I8PeyF2qijfAwtkBTyRBamKq4uxE",
+  });
+  return lib.ajaxSubmit({
+    url: "https://maps.googleapis.com/maps/api/geocode/json" + params,
+    type: "GET",
+  });
+};
+const getLeftCount = async (lng, lat) => {
+  return lib.ajaxSubmit({
+    url: "https://vaccine.kakao.com/api/v2/vaccine/left_count_by_coords",
+    type: "POST",
+    data: JSON.stringify({
+      bottomRight: { x: lng - 0.01, y: lat + 0.02 },
+      onlyLeft: false,
+      order: "latitude",
+      topLeft: { x: lng + 0.01, y: lat - 0.02 },
+    }),
+    beforeSend: (xhr) => {
+      xhr.setRequestHeader("Content-type", "application/json");
+    },
+  });
+};
+const getVaccine = async () => {
   const keyword = $("#position").val();
   $("#position").val("");
   if (keywordItems[keyword]) return;
+  if (Object.keys(keywordItems).length > 4) return;
 
-  const params = lib.jsonToParameter({
-    address: keyword,
-    key: "AIzaSyDDVqnsmm7TCLXzGSQhQPbADOi5NOGmuNo",
-  });
+  const coords = await getCoords(keyword);
+  if (coords.status === "OK") {
+    const location = coords.results[0].geometry.location;
+    if (
+      location.lat < 33 ||
+      location.lat > 43 ||
+      location.lng < 124 ||
+      location.lng > 132
+    ) {
+      return;
+    }
+    const leftList = await getLeftCount(location.lng, location.lat);
+    if (!leftList.organizations.length) {
+      return;
+    }
+    keywordItems[keyword] = true;
 
-  lib.ajaxSubmit({
-    url: "https://maps.googleapis.com/maps/api/geocode/json" + params,
-    type: "GET",
-    callback: (data) => {
-      if (data.status === "OK") {
-        const loadingKeyword = btoa(
-          unescape(encodeURIComponent(keyword))
-        ).replace(/[^a-zA-Z]/g, "");
+    const list = $(".vaccine-list");
+    const content = $("<li>").appendTo(list);
+    $(`<a data-toggle="collapse" href="#collapse-${count}">
+        ${keyword} 주변 확인 중
+        <div class="loading-position loading-${count} active"></div>
+      </a>`).appendTo(content);
+    const collapseItem = $(
+      `<div id="collapse-${count}" class="panel collapse" role="tabpanel">`
+    ).appendTo(content);
+    leftList.organizations.map((item) => {
+      $(`<div class="org-item">
+          <div class="org-name">${item.orgName}</div>
+          <div class="address">${item.address}</div>
+        </div>`).appendTo(collapseItem);
+    });
 
-        const location = data.results[0].geometry.location;
-        const list = $(".vaccine-list");
-        const content = $("<li>").appendTo(list);
-        $(
-          `<a data-toggle="collapse" href="#collapse-${collapseCount}">${keyword} 주변 확인 중<div class="loading-position loading-${loadingKeyword} active"></div></a>`
-        ).appendTo(content);
-        const collapseItem = $(
-          `<div id="collapse-${collapseCount}" class="panel collapse" role="tabpanel">`
-        ).appendTo(content);
+    setInterval(async () => {
+      try {
+        const items = await getLeftCount(location.lng, location.lat);
+        $(`.loading-${count}`).toggleClass("active");
+        items.organizations.map((item) => {
+          if (item.leftCounts) {
+            const successList = $(".success-list");
+            const contentLi = $("<li>").appendTo(successList);
+            $(`<a href="${`https://vaccine.kakao.com/reservation/${item.orgCode}`}" target="_blank">
+                ${keyword} : ${item.orgName}
+              </a>`).appendTo(contentLi);
 
-        setInterval(() => {
-          lib.ajaxSubmit({
-            url: "https://vaccine.kakao.com/api/v2/vaccine/left_count_by_coords",
-            type: "POST",
-            data: JSON.stringify({
-              bottomRight: { x: location.lng - 0.01, y: location.lat + 0.02 },
-              onlyLeft: false,
-              order: "latitude",
-              topLeft: { x: location.lng + 0.01, y: location.lat - 0.02 },
-            }),
-            beforeSend: (xhr) => {
-              xhr.setRequestHeader("Content-type", "application/json");
-            },
-            callback: (data) => {
-              try {
-                $(`.loading-${loadingKeyword}`).toggleClass("active");
-                data.organizations.map((item) => {
-                  if (!keywordItems[keyword]) {
-                    $(
-                      `<div class="org-item"><div class="org-name">${item.orgName}</div><div class="address">${item.address}</div></div>`
-                    ).appendTo(collapseItem);
-                  }
-                  if (item.leftCounts) {
-                    const successList = $(".success-list");
-                    const contentLi = $("<li>").appendTo(successList);
-                    $(
-                      `<a href="${`https://vaccine.kakao.com/reservation/${item.orgCode}?from=KakaoMap&code=VEN00013`}" target="_blank">${keyword} : ${
-                        item.orgName
-                      }</a>`
-                    ).appendTo(contentLi);
-
-                    soundManager.onready(() => {
-                      soundManager.createSound({
-                        id: "mySound",
-                        url: "/doorbell.wav",
-                      });
-                      soundManager.play("mySound");
-                    });
-                    window.open(
-                      `https://vaccine.kakao.com/reservation/${item.orgCode}?from=KakaoMap&code=VEN00013`,
-                      "_blank"
-                    );
-                  }
-                });
-                keywordItems[keyword] = true;
-              } catch (e) {
-                $(`.loading-${loadingKeyword}`).toggleClass("error");
-              }
-            },
-          });
-        }, 1000);
-        collapseCount++;
+            soundManager.onready(() => {
+              soundManager.createSound({
+                id: "mySound",
+                url: "/doorbell.wav",
+                volume: 30,
+              });
+              soundManager.play("mySound");
+            });
+            window.open(
+              `https://vaccine.kakao.com/reservation/${item.orgCode}?from=KakaoMap&code=VEN00013`,
+              "_blank"
+            );
+          }
+        });
+      } catch (e) {
+        $(`.loading-${count}`).toggleClass("error");
       }
-    },
-  });
+    }, 1000);
+    count++;
+  }
 };
